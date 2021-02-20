dc494c5936c6232e5dad977bf84c02930f140246
samkeen <samkeen@lululemon.com>
2021-02-20 07:38:44 -0800

    docs and refactoring

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -80,9 +80,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         var isRunning = Input.GetKey(KeyCode.LeftShift);
-        // this is the direction the input device is indicating in the x/z plane 
-        var inputDirection = DetermineInputDirection();
-        Move(inputDirection, isRunning);
+        Move(isRunning);
         if (!isInDialogue)
         {
             if (Input.GetKeyDown(KeyCode.Space))
@@ -129,9 +127,15 @@ public class PlayerController : MonoBehaviour
         //   "When normalized, a vector keeps the same direction but its length is 1.0"
         return input.normalized;
     }
-
-    private void Move(Vector2 inputDirection, bool running)
+    // 1. determine the direction indicated by input device
+    // 2. if we've entered a dialog while in a jump, simply allow jump to finish (no other movement)
+    // 3. else, calculate the current damped horizontal speed
+    // 4.       Move character (characterController.Move())
+    // 5. if isGrounded, anchor vertical speed to 0
+    private void Move(bool isRunning)
     {
+        // this is the direction the input device is indicating in the x/z plane 
+        var inputDirection = DetermineInputDirection();
         if (!characterController.isGrounded && isInDialogue) // finish jump if entered dialog in midair
         {
             _currentVerticalSpeed += Time.deltaTime * gravity;
@@ -139,10 +143,11 @@ public class PlayerController : MonoBehaviour
         }
         else // perform full movement calculations
         {
-            DampCurrentSpeed(inputDirection, running);
+            _currentHorizontalSpeed = GetDampedCurrentSpeed(inputDirection.magnitude, isRunning);
             ApplyMovement(inputDirection);
-            // update character currentSpeed to actual speed (e.g. 0 if collided with wall)
-            // get the speed (.magnitude) in the x,z plane
+            // update character _currentHorizontalSpeed to actual speed (e.g. 0 if collided with wall)
+            // without this, if you stall at a wall while pressing fwd, _currentHorizontalSpeed stays
+            // at the value for walk|runSpeed
             _currentHorizontalSpeed =
                 new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
         }
@@ -156,9 +161,7 @@ public class PlayerController : MonoBehaviour
 
     private void ApplyMovement(Vector2 inputDirection)
     {
-        var moveDirection = inputDirection != Vector2.zero
-            ? CalculateMoveDirection(inputDirection)
-            : Vector3.one;
+        var moveDirection = CalculateMoveDirection(inputDirection);
         // move the character in the direction they are facing in worldspace
         // adjust y velocity for gravity (gravity is negative)
         _currentVerticalSpeed += Time.deltaTime * gravity;
@@ -168,37 +171,59 @@ public class PlayerController : MonoBehaviour
         characterController.Move(velocity * Time.deltaTime);
     }
 
+    private Vector3 CalculateMoveDirection(Vector2 inputDirection)
+    {
+        // If no indicated direction input, return (1,1,1)
+        if (inputDirection == Vector2.zero)
+        {
+            return Vector3.one;
+        }
+        // Calculating the angle between where we are facing and the indicated input direction
+        // See README:CalculateMoveDirection
+        var targetInputAngle = Mathf.Atan2(inputDirection.x, inputDirection.y) * Mathf.Rad2Deg;
+        // The rotation as Euler angles in degrees 
+        var cameraYAxisRotationAngle = mainCamera.eulerAngles.y;
+        // [+ cameraZAngle] causes the play's forward movement to be that of the camera
+        // targetInputAngle is 0 if there is no movement input, thus targetYAxisRotationAngle will simply be cameraZAngle; the
+        // direction the camera is facing.
+        var blendedCameraAndInputYAxisRotationAngle = targetInputAngle + cameraYAxisRotationAngle;
+        Debug.Log($"Camera Y Axis rotation angle: {cameraYAxisRotationAngle} :: targetInputAngle: {targetInputAngle} :: blendedCameraAndInputYAxisRotationAngle: {blendedCameraAndInputYAxisRotationAngle}");
+        // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        // https://docs.unity3d.com/2020.2/Documentation/Manual/class-Quaternion.html
+        //
+        // see if we can convert to one of these
+        // Unity’s Quaternion class has a number of functions which allow you to create and manipulate rotations
+        // without needing to use Euler angles at all, and these are the ones you should use in most typical cases.
+        // Each of these links to the Script Reference with code samples:
+        //
+        // Creating Rotations:
+        // Quaternion.LookRotation
+        // Quaternion.Angle
+        // Quaternion.AngleAxis
+        // Quaternion.FromToRotation
+        // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        // [* Vector3.forward] converts from Rotation to Direction (https://youtu.be/4HpC--2iowE?t=969)
+        // Example: pre:(0.0, -0.2, 0.0, 1.0), post(multiply by Vector3.forward): (-0.4, 0.0, 0.9)
+        var moveDirection = Quaternion.Euler(0, blendedCameraAndInputYAxisRotationAngle, 0) * Vector3.forward;
+        ApplyDampedYAxisRotation(blendedCameraAndInputYAxisRotationAngle);
+        Debug.Log($"Keyboard input [{inputDirection}], returning moveDirection: {moveDirection}");
+        return moveDirection;
+    }
+
     private float _speedSmoothVelocity;
 
 
-    private void DampCurrentSpeed(Vector2 inputDirection, bool running)
+    private float GetDampedCurrentSpeed(float inputDirectionMagnitude, bool running)
     {
-        _currentHorizontalSpeed = Mathf.SmoothDamp(
+        return Mathf.SmoothDamp(
             _currentHorizontalSpeed,
-            // if inputDirection.magnitude is 0, speed is zero, else the inputDirection.magnitude will be
+            // if inputDirectionMagnitude is 0, speed is zero, else the inputDirection.magnitude will be
             //   1, which will not change the speed
-            (running ? runSpeed : walkSpeed) * inputDirection.magnitude,
+            (running ? runSpeed : walkSpeed) * inputDirectionMagnitude,
             ref _speedSmoothVelocity,
             AirborneAdjustedDampingTime(speedDampTime));
-    }
-
-    private Vector3 CalculateMoveDirection(Vector2 inputDirection)
-    {
-        // determine the character's rotation
-        // (-) = atan(y/x) but in unity we rotate anticlockwise 90deg, so
-        // r = 90 - (-), or r = atan(x/y)
-        // below we could have done Mathf.Atan(input_direction.x/input_direction.y) but Atan2 with 2
-        //   params takes care of division by zero
-        // ------------------------------------------------------------------ y is actually z here
-        // adding 
-        float targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.y)
-                               * Mathf.Rad2Deg
-                               // this causes the play's forward movement to be that of the camera
-                               + this.mainCamera.eulerAngles.y;
-        //
-        var moveDirection = Quaternion.Euler(0, targetRotation, 0) * Vector3.forward;
-        setCharacterRotation(targetRotation);
-        return moveDirection;
     }
 
     // https://docs.unity3d.com/2020.2/Documentation/ScriptReference/Transform-eulerAngles.html
@@ -208,17 +233,20 @@ public class PlayerController : MonoBehaviour
     //    we are doing here
     private float _turnSmoothVelocity;
 
-    private void setCharacterRotation(float targetRotation)
+    private void ApplyDampedYAxisRotation(float targetYAxisRotationAngle)
     {
         // https://docs.unity3d.com/2020.2/Documentation/ScriptReference/Mathf.SmoothDampAngle.html
         // Gradually changes an angle given in degrees towards a desired goal angle over time.
-        transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(
+        var dampedYAxisRotationAngle = Mathf.SmoothDampAngle(
             transform.eulerAngles.y, // current angle
-            targetRotation, // target angle
+            targetYAxisRotationAngle, // target angle
             ref _turnSmoothVelocity, // allow function to reference the _turnSmoothVelocity var
             // time varies on if airport or grounded
-            AirborneAdjustedDampingTime(turnDampTime) // time in sec to perform rotation
-        );
+            AirborneAdjustedDampingTime(turnDampTime)); // time in sec to perform rotation
+        
+        // https://docs.unity3d.com/ScriptReference/Transform-eulerAngles.html
+        // This sets the angle of rotation about the axis
+        transform.eulerAngles = Vector3.up * dampedYAxisRotationAngle;
     }
 
     private void Jump()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Polybrush.sandbox
+# Third.person.movement.sandbox
+
+
+## Player controller notes
+
+### CalculateMoveDirection
+
+![](./Docs/atan2.png)
+Calculating the angle between where we are facing and the indicated input direction
+
+Brackey's [video](https://youtu.be/4HpC--2iowE?t=703) the explains this. (ignore mention of using direction.z vs .y, he is using a vector3)
